52dca8643e97446b69d22a6bff0fa1be0d6c56ef
Ford Hurley <ford.hurley@gmail.com>
2016-01-16 10:37:41 -0500

    Make timeout a flag

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"net/url"
@@ -8,26 +9,48 @@ import (
 	"time"
 )
 
-const timeout = 5 * time.Second
+var timeoutSeconds int
+
+func init() {
+	const (
+		defaultTimeout = 5 // seconds
+		timeoutUsage   = "timeout in seconds"
+	)
+	flag.IntVar(&timeoutSeconds, "timeout", defaultTimeout, timeoutUsage)
+	flag.IntVar(&timeoutSeconds, "t", defaultTimeout, timeoutUsage+" (shorthand)")
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "Usage:\n")
+	fmt.Fprintf(os.Stderr, "  %s [flags] URL\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "Flags:\n")
+	flag.PrintDefaults()
+}
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Fprintf(os.Stderr, "Usage: %s URL\n", os.Args[0])
-		os.Exit(1)
+	flag.Usage = usage
+	flag.Parse()
+
+	// 1 required positional argument
+	if len(flag.Args()) != 1 {
+		usage()
+		os.Exit(2)
 	}
 
-	parsedUrl, err := url.Parse(os.Args[1])
+	rawURL := flag.Args()[0]
+
+	parsedURL, err := url.Parse(rawURL)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
 		os.Exit(1)
 	}
 
-	address := parsedUrl.Host
+	address := parsedURL.Host
 	if address == "" {
-		address = os.Args[1]
+		address = rawURL
 	}
 
-	timeLeft := timeout
+	timeLeft := time.Duration(timeoutSeconds) * time.Second
 	for timeLeft > 0 {
 		start := time.Now()
 		_, err = net.DialTimeout("tcp", address, timeLeft)
